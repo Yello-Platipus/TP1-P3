ac28218826b846f7a36e2cf34ea4b10aa9d47085
RoxyHentai <alexvallejos5b@gmail.com>
2021-12-08 11:56:54 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/es/ucm/tp1/supercars/control/Buyable.java
    #	src/es/ucm/tp1/supercars/control/commands/ShootCommand.java
    #	src/es/ucm/tp1/supercars/control/exceptions/NotEnoughCoinsException.java

---
+++ src/es/ucm/tp1/supercars/control/commands/SaveCommand.java
@@ -0,0 +1,57 @@
+package es.ucm.tp1.supercars.control.commands;
+
+
+import es.ucm.tp1.supercars.control.exceptions.CommandExecuteException;
+import es.ucm.tp1.supercars.control.exceptions.CommandParseException;
+import es.ucm.tp1.supercars.logic.Game;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class SaveCommand extends Command{
+
+    private static final String NAME = "save";
+
+    private static final String DETAILS = "[sa]ve ";
+
+    private static final String SHORTCUT = "sa";
+
+    private static final String HELP = "save the game";
+
+    String nameFile;
+
+    public SaveCommand() {
+        super(NAME, SHORTCUT, DETAILS, HELP);
+    }
+
+    @Override
+    protected Command parse(String[] words) throws CommandParseException {
+        if (matchCommandName(words[0])) {
+            if(words.length == 2){
+                nameFile = words[1];
+                return this;
+            }
+
+            else {
+                throw new CommandParseException(String.format("[ERROR]: Command %s: %s", NAME, INCORRECT_NUMBER_OF_ARGS_MSG));
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean execute(Game game) throws CommandExecuteException {
+        try{
+            BufferedWriter save = new BufferedWriter(new FileWriter(nameFile+".txt"));
+            save.write("Super cars 3.0\n\n");
+            save.write(game.serializer());
+            save.close();
+        }
+        catch (IOException ioe){
+            throw new CommandExecuteException(ioe.getMessage());
+        }
+        System.out.println("Game successfully saved in file "+nameFile+".txt\n");
+        return true;
+    }
+}
